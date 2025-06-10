9a63d8268a4715b071731cd0dba239d12d8d3566
Hakim9969 <hakimyakim200@gmail.com>
2025-06-10 14:33:17 +0300

    Updated

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,11 +2,10 @@ import { DatabaseModule } from './../database/database.module';
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
-import { DatabaseService } from 'src/database/database.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [BooksController],
-  providers: [BooksService, DatabaseService],
+  providers: [BooksService],
 })
 export class BooksModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
 @Module({
